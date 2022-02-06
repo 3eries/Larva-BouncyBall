f6aeaff18aba6bf96b6a3071b4fa5ffc665f8ce6
gmachinee <gmachinee@gmail.com>
2022-02-06 12:11:14 +0900

    [modified] tile.tsx -> tile_108.tsx

--- Resources/content/stage/tile_108.tsx
+++ Resources/content/stage/tile_108.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="tile" tilewidth="108" tileheight="108" tilecount="6" columns="6">
- <image source="tile.png" width="648" height="108"/>
+<tileset version="1.5" tiledversion="1.7.0" name="tile_108" tilewidth="108" tileheight="108" tilecount="6" columns="6">
+ <image source="tile_108.png" width="648" height="108"/>
  <tile id="0">
   <properties>
    <property name="tileType" value="10002"/>
