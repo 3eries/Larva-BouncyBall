9325ccafbd7d796eb1b728b6b704545e651ff7b4
gmachinee <gmachinee@gmail.com>
2022-03-11 14:50:13 +0900

    no message

--- Classes/content/StageManager.cpp
+++ Classes/content/StageManager.cpp
@@ -61,7 +61,7 @@ void StageManager::parseStageFile() {
     
     CCLOG("========== STAGE PARSE START ==========");
     
-    for( int i = 0; i < 20; ++i ) {
+    for( int i = 0; i < 21; ++i ) {
         const auto STAGE = i+1;
         const auto STAGE_FILE = DIR_CONTENT_STAGE + STR_FORMAT("stage_%04d.tmx", STAGE);
         
