8bb128658df835dc3d16d2620826cfc0350ae288
gmachinee <gmachinee@gmail.com>
2022-03-13 06:18:33 +0900

    [added] 월드 1 테마 리소스 적용

--- Resources/content/stage/tile_108.tsx
+++ Resources/content/stage/tile_108.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="tile_108" tilewidth="108" tileheight="108" tilecount="14" columns="14">
- <image source="tile_108.png" width="1512" height="108"/>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="108" tileheight="108" tilecount="25" columns="25">
+ <image source="tile_108.png" width="2700" height="108"/>
  <tile id="0">
   <properties>
    <property name="tile_id" value="00001"/>
@@ -18,55 +18,110 @@
  </tile>
  <tile id="3">
   <properties>
-   <property name="tile_id" value="10001"/>
+   <property name="tile_id" value="01011"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="tile_id" value="10101"/>
+   <property name="tile_id" value="10001"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="tile_id" value="10102"/>
+   <property name="tile_id" value="10002"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="tile_id" value="10201"/>
+   <property name="tile_id" value="10003"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="tile_id" value="10301"/>
+   <property name="tile_id" value="10004"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="tile_id" value="01011"/>
+   <property name="tile_id" value="10101"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
-   <property name="tile_id" value="10251"/>
+   <property name="tile_id" value="10102"/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
-   <property name="tile_id" value="10351"/>
+   <property name="tile_id" value="10103"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="tile_id" value="10352"/>
+   <property name="tile_id" value="10104"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
-   <property name="tile_id" value="10401"/>
+   <property name="tile_id" value="10201"/>
   </properties>
  </tile>
  <tile id="13">
+  <properties>
+   <property name="tile_id" value="10202"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="tile_id" value="10203"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="tile_id" value="10204"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="tile_id" value="10251"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="tile_id" value="10301"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="tile_id" value="10302"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="tile_id" value="10351"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="tile_id" value="10352"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="tile_id" value="10353"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="tile_id" value="10354"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="tile_id" value="10401"/>
+  </properties>
+ </tile>
+ <tile id="24">
   <properties>
    <property name="tile_id" value="10402"/>
   </properties>
