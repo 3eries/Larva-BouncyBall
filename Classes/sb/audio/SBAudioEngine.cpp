360741efe70f1052f9886b138a3983e831912f01
gmachinee <gmachinee@gmail.com>
2022-03-22 10:33:04 +0900

    [added] SBAudioEngine -> SImpleAudioEngine 사용 옵션 추가

--- Classes/sb/audio/SBAudioEngine.cpp
+++ Classes/sb/audio/SBAudioEngine.cpp
@@ -9,7 +9,6 @@
 #include "../base/SBUserDefaultKey.h"
 
 USING_NS_CC;
-using namespace cocos2d::experimental;
 using namespace std;
 
 static SBAudioEngine *instance = nullptr;
@@ -26,6 +25,10 @@ void SBAudioEngine::destroyInstance() {
     
     CC_SAFE_DELETE(instance);
 }
+
+#if USE_SIMPLE_AUDIO_ENGINE == 0
+#pragma mark- AudioEngine
+using namespace cocos2d::experimental;
 
 SBAudioEngine::SBAudioEngine() :
 effectMute(false),
@@ -308,5 +311,228 @@ float SBAudioEngine::getEffectVolume() {
 float SBAudioEngine::getBGMVolume() {
     return getVolume(instance->bgmMute);
 }
+
+#else
+#pragma mark- SimpleAudioEngine
+using namespace CocosDenshion;
+
+#define INVALID_AUDIO_ID        -1
+
+SBAudioEngine::SBAudioEngine() :
+audioEngine(SimpleAudioEngine::getInstance()),
+effectMute(false),
+bgmMute(false),
+paused(false),
+bgm("") {
+}
+
+SBAudioEngine::~SBAudioEngine() {
+    audioEngine->end();
+}
+
+/**
+ * 매니저 초기화
+ */
+void SBAudioEngine::init() {
+    
+    setEffectMute(UserDefault::getInstance()->getBoolForKey(SBUserDefaultKey::EFFECT_MUTE, false));
+    setBGMMute(UserDefault::getInstance()->getBoolForKey(SBUserDefaultKey::BGM_MUTE, false));
+}
+
+/**
+ * mute 설정
+ */
+void SBAudioEngine::setMute(bool mute) {
+    
+    setEffectMute(mute);
+    setBGMMute(mute);
+    
+    // update audio engine
+    /*
+    const float VOLUME = getVolume();
+    
+    for( auto it = _audioIDInfoMap.begin(); it != _audioIDInfoMap.end(); ++it ) {
+        AudioEngine::setVolume(it->first, VOLUME);
+//        _audioEngineImpl->setVolume((int)it->first, VOLUME);
+//        it->second.volume = VOLUME;
+    }
+     */
+}
+
+/**
+ * 효과음 음소거 설정
+ */
+void SBAudioEngine::setEffectMute(bool mute) {
+    
+    this->effectMute = mute;
+    
+    // update user default
+    UserDefault::getInstance()->setBoolForKey(SBUserDefaultKey::EFFECT_MUTE, mute);
+    UserDefault::getInstance()->flush();
+    
+    // update audio engine
+    audioEngine->setEffectsVolume(getVolume(mute));
+}
+
+/**
+ * 배경음 음소거 설정
+ */
+void SBAudioEngine::setBGMMute(bool mute) {
+    
+    this->bgmMute = mute;
+    
+    // update user default
+    UserDefault::getInstance()->setBoolForKey(SBUserDefaultKey::BGM_MUTE, mute);
+    UserDefault::getInstance()->flush();
+    
+    // update audio engine
+    audioEngine->setBackgroundMusicVolume(getVolume(mute));
+}
+
+void SBAudioEngine::preload(const string &file, bool isBGM) {
+    
+    if( isBGM ) {
+        instance->audioEngine->preloadBackgroundMusic(file.c_str());
+    } else {
+        instance->audioEngine->preloadEffect(file.c_str());
+    }
+}
+
+/**
+ * resume
+ */
+void SBAudioEngine::resumeAll() {
+    
+    instance->paused = false;
+    instance->audioEngine->resumeBackgroundMusic();
+    instance->audioEngine->resumeAllEffects();
+}
+
+/**
+ * pause
+ */
+void SBAudioEngine::pauseAll() {
+    
+    instance->paused = true;
+    instance->audioEngine->pauseBackgroundMusic();
+    instance->audioEngine->pauseAllEffects();
+}
+
+/**
+ * 효과음 재생
+ */
+int SBAudioEngine::playEffect(const string &file, bool loop) {
+    
+    if( file == "" ) {
+        return INVALID_AUDIO_ID;
+    }
+    
+    int audioId = instance->audioEngine->playEffect(file.c_str(), loop);
+    instance->audioEngine->setEffectsVolume(getEffectVolume());
+    
+    if( instance->audioPathIDMap.find(file) == instance->audioPathIDMap.end() ) {
+        instance->audioPathIDMap[file] = list<int>();
+    }
+    
+    auto &ids = instance->audioPathIDMap[file];
+    ids.push_back(audioId);
+    
+    return audioId;
+}
+
+void SBAudioEngine::resumeEffect(const string &file) {
+    
+    auto audioIds = getEffectAudioId(file);
+    
+    for( int audioId : audioIds ) {
+        instance->audioEngine->resumeEffect(audioId);
+    }
+}
+
+void SBAudioEngine::pauseEffect(const string &file) {
+    
+    auto audioIds = getEffectAudioId(file);
+    
+    for( int audioId : audioIds ) {
+        instance->audioEngine->pauseEffect(audioId);
+    }
+}
+
+void SBAudioEngine::stopEffect(const string &file) {
+    
+    auto audioIds = getEffectAudioId(file);
+    
+    for( int audioId : audioIds ) {
+        instance->audioEngine->stopEffect(audioId);
+    }
+}
+
+/**
+ * 배경음 재생
+ */
+void SBAudioEngine::playBGM(const string &bgm, bool loop) {
+    
+    if( bgm == "" ) {
+        return;
+    }
+    
+    if( instance->bgm == bgm ) {
+        return;
+    }
+    
+    // 이전 BGM 정지
+    stopBGM();
+    
+    // 새로운 BGM 재생
+    instance->bgm = bgm;
+    instance->audioEngine->playBackgroundMusic(bgm.c_str(), loop);
+    instance->audioEngine->setBackgroundMusicVolume(getBGMVolume());
+}
+
+/**
+ * 배경음 재개
+ */
+void SBAudioEngine::resumeBGM() {
+    instance->audioEngine->resumeBackgroundMusic();
+}
+
+/**
+ * 배경음 일시정지
+ */
+void SBAudioEngine::pauseBGM() {
+    instance->audioEngine->pauseBackgroundMusic();
+}
+
+/**
+ * 배경음 정지
+ */
+void SBAudioEngine::stopBGM() {
+    instance->audioEngine->stopBackgroundMusic();
+    instance->bgm = "";
+}
+
+list<int> SBAudioEngine::getEffectAudioId(const string &file) {
+    
+    auto it = instance->audioPathIDMap.find(file);
+    if( it != instance->audioPathIDMap.end() ) {
+        return it->second;
+    }
+    
+    return list<int>();
+}
+
+float SBAudioEngine::getVolume(bool isMute) {
+    return isMute ? 0 : 1;
+}
+
+float SBAudioEngine::getEffectVolume() {
+    return getVolume(instance->effectMute);
+}
+
+float SBAudioEngine::getBGMVolume() {
+    return getVolume(instance->bgmMute);
+}
+
+#endif // USE_SIMPLE_AUDIO_ENGINE
 
 
--- Classes/sb/audio/SBAudioEngine.hpp
+++ Classes/sb/audio/SBAudioEngine.hpp
@@ -10,9 +10,13 @@
 #include <stdio.h>
 
 #include "cocos2d.h"
-#include "audio/include/AudioEngine.h"
-
 #include "../base/SBTypes.hpp"
+
+#define USE_SIMPLE_AUDIO_ENGINE     0
+
+#if USE_SIMPLE_AUDIO_ENGINE == 0
+#pragma mark- AudioEngine
+#include "audio/include/AudioEngine.h"
 
 class SBAudioEngine : public cocos2d::experimental::AudioEngine {
 public:
@@ -78,6 +82,65 @@ private:
     std::map<std::string, float> durationMap;
 };
 
+#else
+#pragma mark- SimpleAudioEngine
+#include "SimpleAudioEngine.h"
+
+class SBAudioEngine {
+public:
+    static SBAudioEngine* getInstance();
+    static void destroyInstance();
+    ~SBAudioEngine();
+    
+private:
+    SBAudioEngine();
+    
+public:
+    void init();
+    
+    void setMute(bool mute);
+    void setEffectMute(bool mute);
+    void setBGMMute(bool mute);
+    
+public:
+    static void preload(const std::string &file, bool isBGM = true);
+    
+    static void resumeAll();
+    static void pauseAll();
+    
+// Effect
+public:
+    static int  playEffect(const std::string &file, bool loop = false);
+    static void resumeEffect(const std::string &file);
+    static void pauseEffect(const std::string &file);
+    static void stopEffect(const std::string &file);
+    
+// BGM
+public:
+    static void playBGM(const std::string &bgm, bool loop = true);
+    static void resumeBGM();
+    static void pauseBGM();
+    static void stopBGM();
+
+private:
+    static std::list<int> getEffectAudioId(const std::string &file);
+    
+    static float getVolume(bool isMute);
+    static float getEffectVolume();
+    static float getBGMVolume();
+    
+private:
+    CocosDenshion::SimpleAudioEngine *audioEngine;
+    
+    SB_SYNTHESIZE_READONLY_BOOL(effectMute, EffectMute);  // 효과음 음소거 여부
+    SB_SYNTHESIZE_READONLY_BOOL(bgmMute, BGMMute);        // 배경음 음소거 여부
+    SB_SYNTHESIZE_READONLY_BOOL(paused, Paused);          // 일시정지 여부
+    
+    std::unordered_map<std::string,std::list<int>> audioPathIDMap;
+    std::string bgm;
+};
+
+#endif // USE_SIMPLE_AUDIO_ENGINE
 #endif /* SBAudioEngine_hpp */
 
 
